f0ab748a04dc13379b518f8bd8e5ffc651026702
lulumato <luciamatonti390@gmail.com>
2023-11-30 22:27:00 -0300

    actualizacion

--- src/main/java/Main/Mavenproject1.java
+++ src/main/java/Main/Mavenproject1.java
@@ -26,7 +26,6 @@ public class Mavenproject1 {
         } catch (SQLException ex) {
             Logger.getLogger(Mavenproject1.class.getName()).log(Level.SEVERE, null, ex);
         }
-        Equipo1 boca = new Equipo1("Boca Juniors", 11, 6, "Guardiola", true, 0, 0);
         boolean x = true;
 
         do {
@@ -56,57 +55,64 @@ public class Mavenproject1 {
             System.out.println("");
             System.out.println("");
             String opc = sc.next();
-
-            switch (opc) {
-                case "1":
-                    Crear();
-                    break;
-                case "2":
-                    jugar();
-                    break;
-                case "3":
-                    puntos();
-                    break;
-                case "4":
-                    partidosQueSeJugaron();
-                    break;
-                case "5":
-                    estadisticasDeEquipo();
-                    break;
-                case "6":
-                    modificar();
-                    break;
-                case "7":
-                    eliminar();
-                    break;
-                case "8":
-                    darDeAltaUnEquipoEliminado();
-                    break;
-                case "9":
-                    listar();
-                    break;
-                case "10":
-                    System.out.println("Adios!!");
-                    x = false;
-                    break;
-                default:
-                    System.out.println("Opción inválida");
-                    throw new AssertionError();
+            try {
+                switch (opc) {
+                    case "1":
+                        Crear();
+                        break;
+                    case "2":
+                        jugar();
+                        break;
+                    case "3":
+                        puntos();
+                        break;
+                    case "4":
+                        partidosQueSeJugaron();
+                        break;
+                    case "5":
+                        estadisticasDeEquipo();
+                        break;
+                    case "6":
+                        modificar();
+                        break;
+                    case "7":
+                        eliminar();
+                        break;
+                    case "8":
+                        darDeAltaUnEquipoEliminado();
+                        break;
+                    case "9":
+                        listar();
+                        break;
+                    case "10":
+                        System.out.println("Adios!!");
+                        x = false;
+                        break;
+                    default:
+                        System.out.println("Opción inválida");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Error caracter inválido: " + e.getMessage());
+                sc.nextLine();
             }
         } while (x == true);
     }
 
+    // para guardar todos los datos de tipo String se utiliza doble nextLine para corregir un error de Scanner
     public static void Crear() {
         Equipo1 equipo1 = new Equipo1();
         System.out.println("Escriba los datos a completar");
         System.out.println("Nombre equipo: ");
-        equipo1.setNombre(sc.next());
+        equipo1.setNombre(sc.nextLine());
+        equipo1.setNombre(sc.nextLine());
         System.out.println("Cantidad de titulares: ");
         equipo1.setTitulares(sc.nextInt());
         System.out.println("Cantidad de suplentes: ");
         equipo1.setSuplentes(sc.nextInt());
         System.out.println("Nombre DT: ");
-        equipo1.setDirectorTecnico(sc.next());
+        equipo1.setDirectorTecnico(sc.nextLine());
+        equipo1.setDirectorTecnico(sc.nextLine());
         equipo1.setEstado(true);
         equipo1.setPuntos(0);
         equipo1.setPartidosJugados(0);
@@ -116,7 +122,8 @@ public class Mavenproject1 {
 
     private static void modificar() {
         System.out.println("Ingrese el nombre del equipo a modificar: ");
-        String nombre = sc.next();
+        String nombre = sc.nextLine();
+        nombre = sc.nextLine();
         Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombre);
         System.out.println("Ingrese cantidad de puntos: ");
         equipo1.setPuntos(sc.nextInt());
@@ -128,14 +135,16 @@ public class Mavenproject1 {
 
     public static void eliminar() {
         System.out.println("Ingrese el nombre del equipo: ");
-        String nombre = sc.next();
+        String nombre = sc.nextLine();
+        nombre = sc.nextLine();
         Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombre);
         Equipo1Data.desactivarEquipo(equipo1);
     }
 
     public static void darDeAltaUnEquipoEliminado() {
         System.out.println("Ingrese el nombre del equipo: ");
-        String nombre = sc.next();
+        String nombre = sc.nextLine();
+        nombre = sc.nextLine();
         Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombre);
         Equipo1Data.activarEquipo(equipo1);
     }
@@ -149,8 +158,9 @@ public class Mavenproject1 {
 
     public static void jugar() {
         System.out.println("Ingrese el nombre del equipo:");
-        String nombreEquipo = sc.next();
-        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombreEquipo);
+        String nombre = sc.nextLine();
+        nombre = sc.nextLine();
+        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombre);
         if (equipo1 != null) {
             System.out.println("Equipo encontrado: " + equipo1);
 
@@ -162,8 +172,9 @@ public class Mavenproject1 {
 
     public static void puntos() {
         System.out.println("Ingrese el nombre del equipo:");
-        String nombreEquipo = sc.next();
-        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombreEquipo);
+        String nombre = sc.nextLine();
+        nombre = sc.nextLine();
+        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombre);
         if (equipo1 != null) {
             System.out.println("Equipo encontrado: " + equipo1);
             equipo1.puntosTotales();
@@ -174,8 +185,9 @@ public class Mavenproject1 {
 
     public static void partidosQueSeJugaron() {
         System.out.println("Ingrese el nombre del equipo:");
-        String nombreEquipo = sc.next();
-        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombreEquipo);
+        String nombre = sc.nextLine();
+        nombre = sc.nextLine();
+        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombre);
         if (equipo1 != null) {
             System.out.println("Equipo encontrado: " + equipo1);
             equipo1.cantidadDePartidosJugados();
@@ -186,8 +198,9 @@ public class Mavenproject1 {
 
     public static void estadisticasDeEquipo() {
         System.out.println("Ingrese el nombre del equipo:");
-        String nombreEquipo = sc.next();
-        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombreEquipo);
+        String nombre = sc.nextLine();
+        nombre = sc.nextLine();
+        Equipo1 equipo1 = Equipo1Data.buscarEquipoPorNombre(nombre);
         if (equipo1 != null) {
             System.out.println("Equipo encontrado: " + equipo1);
             equipo1.estadistica();
